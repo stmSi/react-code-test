1c05d353749edb80b339518c2da99e55df3cc697
stmSi <stm1998sithumyo@gmail.com>
2023-04-28 23:12:11 +0630

    Added: test cases for App.tsx and PostDetails.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(
+test('renders App component without crashing', () => {
+  render(
     <Provider store={store}>
       <App />
     </Provider>
   );
+});
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+test('renders PostList component at path /', () => {
+  window.history.pushState({}, 'Test page', '/');
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  const postListElement = screen.getByText(/Search Title/i);
+  expect(postListElement).toBeInTheDocument();
 });
---
+++ src/features/post/PostDetails.test.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "../../app/store";
+import { PostDetails } from "./PostDetails";
+import { MemoryRouter, useParams } from "react-router-dom";
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useParams: jest.fn(),
+}));
+
+test("renders PostDetails component", () => {
+  (useParams as jest.Mock).mockReturnValue({ id: "1" });
+
+  render(
+    <Provider store={store}>
+    <MemoryRouter>
+      <PostDetails />
+    </MemoryRouter>
+    </Provider>
+  );
+
+  const returnToPostListButton = screen.getByText(/Return to Post List/i);
+  expect(returnToPostListButton).toBeInTheDocument();
+});
